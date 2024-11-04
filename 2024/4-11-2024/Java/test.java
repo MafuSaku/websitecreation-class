e89a5f1994569ab16c66ebccdf70eb674dd5387a
เอกภพ เชน ซีกัล <58628602+MafuSaku@users.noreply.github.com>
2024-11-04 14:50:31 +0700

    Website for Digital Business Class

---
+++ 2024/4-11-2024/Java/test.java
@@ -0,0 +1,18 @@
+public class test {
+    public static void main(String[] args) {
+        int myNum = 10;
+        System.out.println(myNum);
+
+        float floatingNum = 5.99f;
+        System.out.println(floatingNum);
+
+        char myChar = 'a';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+
+        String myString = "Hello";
+        System.out.println(myString);
+    }
+}
